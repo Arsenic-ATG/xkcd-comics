4f0862f73b3581a6414ffa90a6aba6f86f351149
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-15 18:16:29 +0530

    ➕ Add basic layout of the program

---
+++ downloadXkcd.py
@@ -0,0 +1,11 @@
+#! python3
+
+# downloadXkcd.py - Downloads every single XKCD comic.
+
+import requests, os, bs4
+url = 'http://xkcd.com' 		# starting url
+# TODO: Download the page.
+# TODO: Find the URL of the comic image. # TODO: Download the image.
+# TODO: Save the image to ./xkcd.
+# TODO: Get the Prev button's url.
+print("Done")

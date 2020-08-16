8a1af091915e711c8e187e326c887770c25906d5
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-16 14:45:00 +0530

    ➕ Add folder to store the files

--- downloadXkcd.py
+++ downloadXkcd.py
@@ -4,8 +4,11 @@
 
 import requests, os, bs4
 url = 'http://xkcd.com' 		# starting url
-# TODO: Download the page.
-# TODO: Find the URL of the comic image. # TODO: Download the image.
-# TODO: Save the image to ./xkcd.
-# TODO: Get the Prev button's url.
+os.makedirs('xkcd')		# create a file to place all the downloads
+
+while not url.endswith("#"):
+	# TODO: Download the page.
+	# TODO: Find the URL of the comic image. # TODO: Download the image.
+	# TODO: Save the image to ./xkcd.
+	# TODO: Get the Prev button's url.
 print("Done")

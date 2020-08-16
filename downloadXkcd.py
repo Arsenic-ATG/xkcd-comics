e83ad7ec505822e2b038a02c063fed5e7b4e0b63
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-16 15:48:09 +0530

    🔧Fix : issue #1

--- downloadXkcd.py
+++ downloadXkcd.py
@@ -4,15 +4,15 @@
 
 import requests, os, bs4
 url = 'http://xkcd.com' 		# starting url
-os.makedirs('xkcd')		# create a file to place all the downloads
+os.makedirs('xkcd',exist_ok = True)		# create a file to place all the downloads
 
 while not url.endswith("#"):
 	# Download the page.
 	print("Downloading the page ... ")
 	res = requests.get(url)
 	res.raise_for_status()
-
-	soup = bs4.BeautifulSoup(res,'lxml')
+	print("1 ")
+	soup = bs4.BeautifulSoup(res.text,'lxml')
 
 	# Find the URL of the comic image.
 	comic_element = soup.select('#comic img')
@@ -21,18 +21,18 @@ while not url.endswith("#"):
 	else:
 		comic_image_url = comic_element[0].get('scr')
 		# download the image
-		print("Downloading the imgae %s .. " %(comic_image_url))
+		print("Downloading the image %s .. " %(comic_image_url))
 		res = requests.get(comic_image_url)
 		res.raise_for_status()
 
-	# Save the image to ./xkcd.
-	file = os.open(os.join('xkcd',os.path.basename(comic_image_url)) , 'wb')
-	for chunck in res.iter_content(10000):
-		file.write(chunck)
-	file.close()
+		# Save the image to ./xkcd.
+		file = os.open(os.join('xkcd',os.path.basename(comic_image_url)) , 'wb')
+		for chunck in res.iter_content(10000):
+			file.write(chunck)
+		file.close()
 
 	# Get the Prev button's url.
 	prev_link = soup.select('a[rel="prev"]')[0]
 	url = 'http://xkcd.com' + prev_link.get('href')
-	
+
 print("Done")
